78a4b479c0600ec8506103bdb7e2bb85a094944b
ramiro-l <ramideporte@gmail.com>
2024-12-22 21:41:07 -0300

    fix: router lang.

--- src/components/toggle-lang.tsx
+++ src/components/toggle-lang.tsx
@@ -27,7 +27,7 @@ const LangButton = ({
       onClick={() => {
         const url = window.location.href.replace(window.location.origin, "");
         if (lang !== currentLang) {
-          router.push(url.replace(`/${currentLang}`, `/${lang}`));
+          router.replace(url.replace(`/${currentLang}`, `/${lang}`));
         }
       }}
       className={`min-w-7 rounded-md p-1.5 uppercase hover:bg-muted/40 hover:text-secondary-foreground ${currentLang === lang ? "!bg-muted text-secondary-foreground" : ""}`}
